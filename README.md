5c49753916bad4888d0a97ad96638c6d47b26a98
sakthi49 <sakthikumaran.1997@gmail.com>
2024-06-29 19:57:18 +0530

    Added Project

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Interview_Question_Creator_Project
+
+#### How to run this project
+
+1. create an environment
+
+''' bash
+conda create -n interview python=3.10 -y
+
+conda activate interview
+'''
+2. Install requirements
+
+'''bash
+pip install -r requirements.txt
+'''
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "INTERVIEW_QUESTION_CREATION_PROJECT",
+    version= "0.0.0",
+    author= "Sakthikumaran",
+    author_email= "sakthikumaran.1997@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
